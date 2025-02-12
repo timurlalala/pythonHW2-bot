57e4f142705f5e97773f3d8c725dc77d231d791c
Тимур Самигуллин <timursamwork@gmail.com>
2025-02-12 09:04:33 +0300

    bug fix

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command, StateFilter
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from states import *
-from templates import MessageTemplates
+from templates import SetProfileMessageTemplates
 from keyboards import *
 from handlers.utils import generate_profile_summary
 
@@ -11,12 +11,12 @@ rt = Router()
 
 @rt.message(Command('start'))
 async def start(message: Message, state: FSMContext):
-    await message.answer(MessageTemplates.WELCOME)
+    await message.answer(SetProfileMessageTemplates.WELCOME)
     await state.set_state(SetProfile.just_started)
 
 @rt.message(Command("set_profile"), StateFilter(SetProfile.just_started))
 async def set_profile(message: Message, state: FSMContext):
-    await message.answer(MessageTemplates.WEIGHT_REQUEST)
+    await message.answer(SetProfileMessageTemplates.WEIGHT_REQUEST)
     await state.set_state(SetProfile.setting_weight)
 
 @rt.message(StateFilter(SetProfile.setting_weight))
@@ -25,11 +25,11 @@ async def set_weight(message: Message, state: FSMContext):
         weight = int(message.text)
         assert 15 < weight < 500
     except (ValueError, AssertionError):
-        await message.answer(MessageTemplates.WEIGHT_ERROR)
+        await message.answer(SetProfileMessageTemplates.WEIGHT_ERROR)
         return
     await state.update_data({'weight': weight})
 
-    await message.answer(MessageTemplates.HEIGHT_REQUEST)
+    await message.answer(SetProfileMessageTemplates.HEIGHT_REQUEST)
     await state.set_state(SetProfile.setting_height)
 
 @rt.message(StateFilter(SetProfile.setting_height))
@@ -38,11 +38,11 @@ async def set_height(message: Message, state: FSMContext):
         height = int(message.text)
         assert 30 < height < 250
     except (ValueError, AssertionError):
-        await message.answer(MessageTemplates.HEIGHT_ERROR)
+        await message.answer(SetProfileMessageTemplates.HEIGHT_ERROR)
         return
     await state.update_data({'height': height})
 
-    await message.answer(MessageTemplates.AGE_REQUEST)
+    await message.answer(SetProfileMessageTemplates.AGE_REQUEST)
     await state.set_state(SetProfile.setting_age)
 
 @rt.message(StateFilter(SetProfile.setting_age))
@@ -51,11 +51,11 @@ async def set_age(message: Message, state: FSMContext):
         age = int(message.text)
         assert 12 < age < 120
     except (ValueError, AssertionError):
-        await message.answer(MessageTemplates.AGE_ERROR)
+        await message.answer(SetProfileMessageTemplates.AGE_ERROR)
         return
     await state.update_data({'age': age})
 
-    await message.answer(MessageTemplates.ACTIVITY_REQUEST)
+    await message.answer(SetProfileMessageTemplates.ACTIVITY_REQUEST)
     await state.set_state(SetProfile.setting_activity_rate)
 
 @rt.message(StateFilter(SetProfile.setting_activity_rate))
@@ -64,11 +64,11 @@ async def set_activity_rate(message: Message, state: FSMContext):
         activity_rate = int(message.text)
         assert activity_rate < 1000
     except ValueError:
-        await message.answer(MessageTemplates.ACTIVITY_ERROR)
+        await message.answer(SetProfileMessageTemplates.ACTIVITY_ERROR)
         return
     await state.update_data({'activity_rate': activity_rate})
 
-    await message.answer(MessageTemplates.CITY_REQUEST)
+    await message.answer(SetProfileMessageTemplates.CITY_REQUEST)
     await state.set_state(SetProfile.setting_city)
 
 @rt.message(StateFilter(SetProfile.setting_city))
@@ -88,7 +88,7 @@ async def set_city(message: Message, state: FSMContext):
     calories = (10*state_data['weight'] + 6.25*state_data['height'] - 5*state_data['age']) * act_level
     await state.update_data({'target_calories': calories})
     await message.answer(
-        MessageTemplates.TARGET_CALORIES_CALCULATED.format(calories=calories),
+        SetProfileMessageTemplates.TARGET_CALORIES_CALCULATED.format(calories=calories),
         reply_markup=target_confirmation_keyboard, )
     await state.set_state(SetProfile.confirming_target_calories)
 
@@ -97,7 +97,7 @@ async def set_city(message: Message, state: FSMContext):
 @rt.message(StateFilter(SetProfile.confirming_target_calories), F.text == 'Ввести свою цель')
 async def request_calories(message: Message, state: FSMContext):
 
-    await message.answer(MessageTemplates.TARGET_CALORIES_REQUEST)
+    await message.answer(SetProfileMessageTemplates.TARGET_CALORIES_REQUEST)
     await state.set_state(SetProfile.setting_target_calories)
 
 @rt.message(StateFilter(SetProfile.setting_target_calories))
@@ -106,12 +106,12 @@ async def set_calories(message: Message, state: FSMContext):
         calories = int(message.text)
         assert calories > 1500
     except (ValueError, AssertionError):
-        await message.answer(MessageTemplates.TARGET_CALORIES_ERROR)
+        await message.answer(SetProfileMessageTemplates.TARGET_CALORIES_ERROR)
         return
     await state.update_data({'target_calories': calories, 'target_set_by_user': True})
 
     state_data = await state.get_data()
-    await message.answer(generate_profile_summary(state_data))
+    await message.answer(SetProfileMessageTemplates.SETTING_DONE + generate_profile_summary(state_data))
     await state.set_state(SetProfile.profile_is_set)
 
 @rt.message(StateFilter(SetProfile.confirming_target_calories), F.text == 'Сохранить')
@@ -120,7 +120,16 @@ async def request_calories(message: Message, state: FSMContext):
     await state.update_data({'target_set_by_user': False})
 
     state_data = await state.get_data()
-    await message.answer(generate_profile_summary(state_data))
+    await message.answer(SetProfileMessageTemplates.SETTING_DONE + generate_profile_summary(state_data))
     await state.set_state(SetProfile.profile_is_set)
+
+@rt.message(Command('show_profile'))
+async def show_profile(message: Message, state: FSMContext):
+
+    state_data = await state.get_data()
+    await message.answer("Ваш профиль: " + generate_profile_summary(state_data))
+
+
+
 
 # TODO Сделать редактирование профиля
--- handlers/utils.py
+++ handlers/utils.py
@@ -1,6 +1,6 @@
-from aiogram.fsm.context import FSMContext
-from templates import MessageTemplates
+from templates import SetProfileMessageTemplates
 from typing import Dict, Any
 
 def generate_profile_summary(data: Dict[str, Any]):
-    return MessageTemplates.PROFILE_SUMMARY.format(**data)
+    return SetProfileMessageTemplates.PROFILE_SUMMARY.format(**data)
+
